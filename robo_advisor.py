70b2927059c22150caced1cb6da4972f65426140
datright <84527629+datright@users.noreply.github.com>
2021-06-10 20:25:50 -0400

    Getting data from API Alpha Advantage

--- robo_advisor.py
+++ robo_advisor.py
@@ -1,1 +1,17 @@
 print("TO DO: Make a web request")
+
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv() # go get env vars from the .env file
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+symbol = "MSFT"
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
